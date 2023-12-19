a26b3144d8bbe78074940a4ee3681245e80aec18
luizefz <luizefsilva2@gmail.com>
2023-12-19 13:53:33 -0300

    feat: adesão do challenge Fill.asm - Pintar a tela

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,40 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+    @SCREEN //16384 Caractere dedicado a tela em assembly
+    D=A
+    @i
+    M=D          //i = SCREEN
+
+(LOOP)           //while(i < screen - 1)
+    @i
+    D=M
+    @24575       //Ultima posicao da memoria refenrente a tela do computador
+    D=A-D
+    @START
+    D;JLT       // i<screen -1(negative check)
+    @KBD
+    D=M
+    @BLACKEN    // se uma das teclas do teclado for pressionada, vai para BLACKEN
+    D;JNE
+
+    @i
+    D=M
+    A=D
+    M=0 //se não, ele continua pintando a tela de branco
+    @CONTINUE
+    0;JMP
+
+(BLACKEN)
+    @i
+    D=M
+    A=D
+    M=-1 //Aqui ele muda a cor do pincel para preto == -1 (1111111111111111)
+
+(CONTINUE)
+    @i
+    M=M+1 //i++
+    @LOOP
+    0;JMP
